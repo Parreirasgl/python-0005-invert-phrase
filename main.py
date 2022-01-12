932a0439bf6ad8d7c4a14c0aa8c8b0b3b290a1ba
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-12 01:33:05 -0300

    Refactoring using functions (list, invert, join, delete and invert)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Inverter uma palavra sem usar funções nativas list or join or reverse.
-# Invert a word without using native functions called list or join or reverse.
+# Inverter uma palavra sem usar funções nativas.
+# Invert a word without using native functions.
 
 def word_to_list(word):
     list_word = []
@@ -7,12 +7,36 @@ def word_to_list(word):
         list_word.append(i)
     return list_word
 
+def delete_item(list, position):
+    new_list = []
+    for i4 in range(len(list)):
+        if position == i4:
+            pass
+        else:
+            new_list.append(list[i4])
+    return new_list
+
+def insert_item(list, position, item):
+    new_list = []
+    for i5 in range(len(list) + 1):
+        if i5 == len(list):
+            if position == i5:
+                new_list.append(item)
+            else:
+                pass
+        elif position == i5:
+            new_list.append(item)
+            new_list.append(list[i5])
+        else:
+            new_list.append(list[i5])
+    return new_list
+
 def invert_list(list_word):
     length = len(list_word)
     for i2 in range(length):
         last_letter = list_word[length - 1]
-        list_word.pop(length - 1)
-        list_word.insert(i2, last_letter)
+        list_word = delete_item(list_word, length-1)
+        list_word = insert_item(list_word, i2, last_letter)
     return list_word
 
 def join_list(list_word):
