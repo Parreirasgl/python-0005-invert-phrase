33e1f68fa7209e967644b545384ddebdeb10ca31
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-11 23:44:03 -0300

    Refactoring using functions

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-# Inverter uma palavra sem usar função reverse
-# Invert a word without using reverse function
+# Inverter uma palavra sem usar funções nativas list and reverse.
+# Invert a word without using native function called list and reverse.
+
+def word_to_list(word):
+    list_word = []
+    for i in word:
+        list_word.append(i)
+    return list_word
+
+def invert_list(list_word):
+    length = len(list_word)
+    for i2 in range(length):
+        last_letter = list_word[length - 1]
+        list_word.pop(length - 1)
+        list_word.insert(i2, last_letter)
+    return list_word
+
 word = input()
-list_word = list(word)
-length = len(word)
-
-for i in range(length):
-    last_letter = list_word[length - 1]
-    list_word.pop(length - 1)
-    list_word.insert(i, last_letter)
-
-new_word = "".join(list_word)
+new_word = "".join(invert_list(word_to_list(word)))
 print(new_word)
 
 
