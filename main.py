8223d066cadbb0055e8e8bbb5d77d5ffd2f97525
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-11 23:19:27 -0300

    It's working

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # Inverter uma palavra sem usar função reverse
 # Invert a word without using reverse function
-
 word = input()
 list_word = list(word)
 length = len(word)
+
+for i in range(length):
+    last_letter = list_word[length - 1]
+    list_word.pop(length - 1)
+    list_word.insert(i, last_letter)
+
+new_word = "".join(list_word)
+print(new_word)
+
 
 
