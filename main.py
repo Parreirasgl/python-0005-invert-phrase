f3a50d661991e330d93225342134c571b1d28846
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-12 02:01:55 -0300

    New comments.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 
 def word_to_list(word):
     """Transform word to list.
-    Transforma palavra em lista"""
+    Transforma palavra em lista.
+    """
     list_word = []
     for i in word:
         list_word.append(i)
@@ -12,7 +13,8 @@ def word_to_list(word):
 
 def delete_item(list, position):
     """Delete item from list.
-    Deleta item da lista"""
+    Deleta item da lista.
+    """
     new_list = []
     for i4 in range(len(list)):
         if position == i4:
@@ -23,12 +25,13 @@ def delete_item(list, position):
 
 
 def insert_item(list, position, item):
-    """Insert item list.
+    """Insert item in list.
     If item is inserted in the middle of the list, it push others.
     The item can be inserted after the last item in the list.
     Insere item em lista.
     Se item é iserido no meio da lista, empurra outros.
-    O item pode ser inserido após o último item da lista."""
+    O item pode ser inserido após o último item da lista.
+    """
     new_list = []
     for i5 in range(len(list) + 1):
         if i5 == len(list):
@@ -45,10 +48,12 @@ def insert_item(list, position, item):
 
 
 def invert_list(list_word):
-    """Invert items in list. To do this, it does a for loop through the length of the list.
+    """Invert items in list.
+    To do this, it does a for loop through the length of the list.
     Then save the last value of the list; then deletes last value;
     then insert last value in the right place
-    Inverte itens na lista. Para isso, faz um for loop pelo comprimento da lista.
+    Inverte itens na lista.
+    Para isso, faz um for loop pelo comprimento da lista.
     Depois guarda último valor da lista; depois deleta último valor;
     depois insere último valor no lugar certo.
     """
@@ -62,7 +67,8 @@ def invert_list(list_word):
 
 def join_list(list_word):
     """Turns list into word.
-    Transforma lista em palavra."""
+    Transforma lista em palavra.
+    """
     new_word = ""
     for i3 in list_word:
         new_word += i3
